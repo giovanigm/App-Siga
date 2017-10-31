b10b0d5a5603ad6a57dee557d05d07165801453d
Giovani <ggmelone@gmail.com>
2017-10-31 13:10:41 -0200

    TelaDisciplinas funcionando via json

--- src/modules/tela-disciplinas/tela-disciplinas.jsx.js
+++ src/modules/tela-disciplinas/tela-disciplinas.jsx.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Container, Content, Body, Card, CardItem, Text, H1, H2 } from 'native-base';
+import { Container, Content, List, ListItem, Left, Item, CardItem, Text, H1, H2 } from 'native-base';
 
 import styles from './styles';
+
+
+const disciplinas = [
+    {
+        codigo: 'HSO003',
+        nome: 'Ética e Responsabilidade Profissional',
+    },
+    {
+        codigo: 'IIA002',
+        nome: 'Inteligência Artificial',
+    },
+    {
+        codigo: 'ITI004',
+        nome: 'Gestão e Governança de Tecnologia da Informação',
+    },
+    {
+        codigo: 'LIN600',
+        nome: 'Inglês VI',
+    },
+    {
+        codigo: 'TES001',
+        nome: 'Estágio Supervisionado',
+    },
+    {
+        codigo: 'TTG103',
+        nome: 'Trabalho de Graduação II',
+    },
+];
 
 class TelaDisciplinas extends Component {
     static navigatorStyle = styles.navigatorStyle;
@@ -15,11 +43,15 @@ class TelaDisciplinas extends Component {
         return (
             <Container style={styles.container}>
                 <Content>
-                    <Card style={styles.card}>
-                        <CardItem header>
-                            <H1>Disciplinas do Aluno</H1>
-                        </CardItem>
-                    </Card>
+                    <List
+                        dataArray={disciplinas}
+                        renderRow={data =>
+                            (<ListItem style={styles.item}>
+                                <Left>
+                                    <Text style={styles.text}>{data.codigo}</Text>
+                                    <Text style={styles.text}>{data.nome}</Text>
+                                </Left>
+                            </ListItem>)} />
                 </Content>
             </Container>
         );
